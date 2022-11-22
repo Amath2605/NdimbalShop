0f9db673e562997e32ea352d53f51b7d05d17362
[Amath Diatta] <amadoudiatta734@gmail.com>
2022-11-22 21:13:06 +0000

    modifier

---
+++ lib/core/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  const AppColor._();
+
+  static const lightOrange = Color.fromARGB(255, 60, 250, 215);
+  static const lightBlack = Color(0xFF101725);
+}
